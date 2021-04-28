e2011c819e7e439bcd96b2f502b18fce89935d23
Brett V. Forsgren <brett.forsgren@outlook.com>
2021-04-28 09:23:41 -0700

    update test runner to net5.0

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -1,38 +1,46 @@
 @echo off
 setlocal
 
-set thisdir=%~dp0
 set configuration=Debug
 set runtests=true
 
 :parseargs
 if "%1" == "" goto argsdone
-if /i "%1" == "-c" (
-    set configuration=%2
-    shift
-    shift
-    goto parseargs
-)
-if /i "%1" == "-notest" (
-    set runtests=false
-    shift
-    goto parseargs
-)
+if /i "%1" == "-c" goto set_configuration
+if /i "%1" == "--configuration" goto set_configuration
+if /i "%1" == "-notest" goto set_notest
+if /i "%1" == "--notest" goto set_notest
 
 echo Unsupported argument: %1
 goto error
 
+:set_configuration
+set configuration=%2
+shift
+shift
+goto parseargs
+
+:set_notest
+set runtests=false
+shift
+goto parseargs
+
 :argsdone
 
 :: build
-set SOLUTION=%~dp0IxMilia.Shx.sln
-dotnet restore %SOLUTION%
+dotnet restore
 if errorlevel 1 exit /b 1
-dotnet build %SOLUTION% -c %configuration%
+dotnet build -c %configuration%
 if errorlevel 1 exit /b 1
 
 :: test
 if /i "%runtests%" == "true" (
-    dotnet test "%SOLUTION%" -c %configuration% --no-restore --no-build
+    dotnet test -c %configuration% --no-restore --no-build
     if errorlevel 1 goto error
 )
+
+goto :eof
+
+:error
+echo Error building project.
+exit /b 1
